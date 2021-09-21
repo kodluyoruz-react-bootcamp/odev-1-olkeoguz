4a6eee3a379b11038b06a7836bf7b838a3931dca
Oğuz Ölke <oguzolke@gmail.com>
2021-09-21 21:39:33 +0300

    a comment added

--- src/lib/service.js
+++ src/lib/service.js
@@ -1,11 +1,16 @@
-import axios from "axios";
+import axios from 'axios';
 
-export default async function getData(userId){
-   try {
-    const {data:user} = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
-    const {data:posts} =await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
-    return {user,posts};
-   } catch (error) {
-       console.log(error.message);
-   }
+export default async function getData(userId) {
+  try {
+    //getting the user and the posts data
+    const { data: user } = await axios.get(
+      `https://jsonplaceholder.typicode.com/users/${userId}`
+    );
+    const { data: posts } = await axios.get(
+      `https://jsonplaceholder.typicode.com/posts?userId=${userId}`
+    );
+    return { user, posts };
+  } catch (error) {
+    console.log(error.message);
+  }
 }
